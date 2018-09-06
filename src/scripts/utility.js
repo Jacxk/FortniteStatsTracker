9d2deccbf731bd6d3693d2a2274c4c524fc9eb42
Yanluis Fermin <32645451+Jacxk@users.noreply.github.com>
2018-09-05 20:19:41 -0400

    Revert "App recoded and made better"

---
+++ src/scripts/utility.js
@@ -0,0 +1,64 @@
+document.getElementById('image').ondragstart = () => {
+    return false;
+};
+
+// Side menu
+let isOpen = false;
+
+function openNav() {
+    if (!isOpen) {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("header").style.opacity = "0.5";
+        document.getElementById("mainBlock").style.opacity = "0.5";
+        isOpen = true
+    } else {
+        closeNav();
+        isOpen = false
+    }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("header").style.opacity = "1";
+    document.getElementById("mainBlock").style.opacity = "1";
+    isOpen = false
+}
+
+function openSource() {
+    require('electron').shell.openExternal("https://github.com/Jacxk/FortniteStatsTracker")
+}
+
+$(document).mouseup((e) => {
+    if (!isOpen) return;
+    const container = $("#mySidenav");
+
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        closeNav();
+    }
+});
+
+// logout
+function returnLogin() {
+    window.location = 'login.html';
+}
+
+// options
+function options() {
+    $("#body").load("settings.html");
+}
+
+function home() {
+    $("#body").load("stats.html");
+}
+
+// Send Alert
+module.exports.sendAlert = (alert) => {
+    const html = `<div class="alert"><span class="closebtn" onclick="this.parentElement.style.display='none';">` +
+        `&times;</span>` + alert + `</div>`;
+
+    $('#sendAlert').html(html);
+
+    setTimeout(() => {
+        $('#sendAlert').html(null);
+    }, 5000);
+};
